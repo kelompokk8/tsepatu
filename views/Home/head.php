70190dd9ea8fb8c3ec8b402ae917e6044fa5f4c0
icemr4 <43738451+icemr4@users.noreply.github.com>
2018-10-29 14:28:22 +0700

    Add files via upload

---
+++ views/Home/head.php
@@ -0,0 +1,45 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html class="no-js" lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Olshop Sepatu</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="<?php echo base_url().'assets/subas/img/icon/favicon.png'?>">
+
+    <!-- All CSS Files -->
+    <!-- Bootstrap fremwork main css -->
+    <link rel="stylesheet" href="<?php echo base_url().'assets/subas/css/bootstrap.min.css'?>">
+    <!-- Nivo-slider css -->
+    <link rel="stylesheet" href="<?php echo base_url().'assets/subas/lib/css/nivo-slider.css'?>">
+    <link rel="stylesheet" href="<?php echo base_url().'assets/subas/lib/js/jquery.nino.slider.js'?>">
+    <!-- This core.css file contents all plugings css file. -->
+    <link rel="stylesheet" href="<?php echo base_url().'assets/subas/css/core.css'?>">
+    <!-- Theme shortcodes/elements style -->
+    <link rel="stylesheet" href="<?php echo base_url().'assets/subas/css/shortcode/shortcodes.css'?>">
+    <link rel="stylesheet" href="<?php echo base_url().'assets/subas/css/shortcode/footer.css'?>">
+    <!-- Theme main style -->
+    <link rel="stylesheet" href="<?php echo base_url().'assets/subas/style.css'?>">
+    <!-- Responsive css -->
+    <link rel="stylesheet" href="<?php echo base_url().'assets/subas/css/responsive.css'?>">
+    <!-- Template color css -->
+    <link href="<?php echo base_url().'assets/subas/css/color/color-core.css'?>" data-style="styles" rel="stylesheet">
+    <!-- User style -->
+    <link rel="stylesheet" href="<?php echo base_url().'assets/subas/css/custom.css'?>">
+    <!-- icon -->
+    <link rel="stylesheet" href="<?php echo base_url().'assets/subas/fonts/Material-Design-Iconic-Font.eot'?>">
+    <link rel="stylesheet" href="<?php echo base_url().'assets/subas/fonts/Material-Design-Iconic-Font.svg'?>">
+    <link rel="stylesheet" href="<?php echo base_url().'assets/subas/fonts/Material-Design-Iconic-Font.ttf'?>">
+    <link rel="stylesheet" href="<?php echo base_url().'assets/subas/fonts/Material-Design-Iconic-Font.woff'?>">
+    <link rel="stylesheet" href="<?php echo base_url().'assets/subas/fonts/Material-Design-Iconic-Font.woff2'?>">
+
+    <!-- Modernizr JS -->
+    <script src="assets/subas/js/vendor/modernizr-2.8.3.min.js"></script>
+</head>
+
+<body>
---
+++ views/Home/navbar.php
@@ -0,0 +1,64 @@
+<!-- Body main wrapper start -->
+    <div class="wrapper">
+
+        <!-- START HEADER AREA -->
+        <header class="header-area header-wrapper">
+            <!-- header-top-bar -->
+            <div class="header-top-bar plr-185">
+                <div class="container-fluid">
+                    <div class="row"></div>
+                </div>
+            </div>
+
+            <!-- header-middle-area -->
+            <div id="sticky-header" class="header-middle-area plr-185">
+                <div class="container-fluid">
+                    <div class="full-width-mega-dropdown">
+                        <div class="row">
+                            <!-- logo -->
+                            <div class="col-md-2 col-sm-6 col-xs-12">
+                                <div class="logo">
+                                    <a href="<?php echo base_url().'index.html'?>">
+                                        <img src="<?php echo base_url().'assets/subas/img/logo/logo.png'?>" alt="main logo"></a>
+                                </div>
+                            </div>
+                            <!-- primary-menu -->
+                            <div class="col-md-8 hidden-sm hidden-xs">
+                                <nav id="primary-menu">
+                                    <ul class="main-menu text-center">
+                                        <li><a href="<?php echo base_url('home');?>">Home</a></li>
+                                        <li><a href="<?php echo base_url('blog.html');?>">Blog</a></li>
+                                        <li><a href="<?php echo base_url('about.html');?>">Kategori</a>
+                                        </li>
+                                        <li><a href="<?php echo base_url('contact.html');?>">About Us</a>
+                                        </li>
+                                        <li><a href="<?php echo base_url('contact.html');?>">Contact</a>
+                                        </li>
+                                    </ul>
+                                </nav>
+                            </div>
+                            <!-- header-search & total-cart -->
+                            <div class="search-top-cart  f-right">
+                                <!-- header-search -->
+                                <div class="header-search f-left">
+                                    <div class="header-search-inner">
+                                        <button class="search-toggle">
+                                            <i class="zmdi zmdi-search"></i>
+                                        </button>
+                                        <form action="#">
+                                            <div class="top-search-box">
+                                                <input type="text" placeholder="Search here your product...">
+                                                <button type="submit">
+                                                    <i class="zmdi zmdi-search"></i>
+                                                </button>
+                                            </div>
+                                        </form> 
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- END HEADER AREA -->
